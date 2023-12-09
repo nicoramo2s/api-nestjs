0d87c0eccfdf3eb91ec4905a4059f923312dec2f
nicoramo2s <nicolasramo2s@hotmail.com>
2023-12-09 09:59:39 -0300

    add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/final-project-bootcamp'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASES_URL),
     UsersModule,
     PostsModule,
     AuthModule,
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
